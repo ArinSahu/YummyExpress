911fb3bbd153dba7787cfe8d771e02dcd51c880c
ArinSahu <arinsahu0@gmail.com>
2025-04-18 05:32:18 +0530

    creating-user

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://arinsahu0:pragatikimkc@cluster0.behds7e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoURI = 'mongodb+srv://arinsahu0:lodu@cluster0.behds7e.mongodb.net/ChomatoMERN?retryWrites=true&w=majority&appName=Cluster0';
 
 async function mongoDB() {
     try {
         await mongoose.connect(mongoURI);
         console.log("MongoDB connected");
+       
+        const fetched_data = await mongoose.connection.db.collection("food_items").find({}).toArray();
+        // console.log("Fetched Food Items:", fetched_data);
     } catch (err) {
         console.error("MongoDB connection error:", err);
     }
 }
-
 
 module.exports = mongoDB;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,10 +20,13 @@ export default function Navbar() {
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <Link className="nav-link active" to="/">Home</Link>
+                                <Link className="nav-link" to="/">Home</Link>
                             </li>
                             <li className="nav-item">
                                 <Link className="nav-link" to="/login">Login</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/login">SignUp</Link>
                             </li>
                            
                         </ul>
