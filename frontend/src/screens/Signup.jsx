dc37c8565a4e48f5e7e9286520be962fba8c8031
Arin Sahu <120007631+ArinSahu@users.noreply.github.com>
2025-08-08 15:48:10 +0530

    Update Signup.jsx

--- frontend/src/screens/Signup.jsx
+++ frontend/src/screens/Signup.jsx
@@ -15,7 +15,7 @@ export default function Signup() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("https://yummyexpressfrontend.onrender.com/api/createuser", {
+    const response = await fetch("https://yummyexpress-backend.onrender.com/api/createuser", {
       method: "POST",
       headers: {
         'Content-Type': 'application/json'
