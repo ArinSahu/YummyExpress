544f141daeadc16f8813c73295eed227de1dd1a2
Arin Sahu <120007631+ArinSahu@users.noreply.github.com>
2025-08-08 15:47:43 +0530

    Update Login.jsx

--- frontend/src/screens/Login.jsx
+++ frontend/src/screens/Login.jsx
@@ -9,7 +9,7 @@ export default function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("https://yummyexpressfrontend.onrender.com/api/loginuser", {
+    const response = await fetch("https://yummyexpress-backend.onrender.com/api/loginuser", {
       method: "POST",
       headers: {
         'Content-Type': 'application/json'
